b396285af7d780dd2ea0a7a2a63a04add1d76b02
MBennin <matthewbennin@gmail.com>
2020-08-02 22:23:14 -0700

    Finished Make Phrase

--- R/make_phrase.R
+++ R/make_phrase.R
@@ -22,8 +22,27 @@ make_phrase <- function(num, num_word, item, verb, adjective, location){
 
   verb <- str_replace_na(verb, "")
 
-  #????
+  adjective <- str_replace_na(adjective, "")
 
+  location <- str_replace_na(location, "")
+
+  output <- num_word
+
+  if(adjective != ""){
+    output <- paste(output, adjective, sep = " ")
+  }
+
+  output <- paste(output, item, sep = " ")
+
+  if(verb != ""){
+    output <- paste(output, verb, sep = " ")
+  }
+
+  if(location != ""){
+    output <- paste(output, location, sep = " ")
+  }
+
+  return(output)
 
 }
 
